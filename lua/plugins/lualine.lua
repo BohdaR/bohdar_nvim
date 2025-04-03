06768c49793e74cd3c81aaaf844deb19b1097250
BohdaR <bohdanshushval@gmail.com>
2025-04-04 00:07:15 +0300

    Add AI status to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,15 @@ return {
     require('lualine').setup({
       options = {
         theme = 'dracula',
-      }
+      },
+      sections = {
+        lualine_x = {
+          require('minuet.lualine'),
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+      },
     })
   end
 }
