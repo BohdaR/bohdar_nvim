ef9d99d15672a4ca745971db74a9957826d400ba
BohdaR <bohdanshushval@gmail.com>
2025-05-09 00:15:35 +0300

    Add shortcuts for buffer switching commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,5 @@
-vim.keymap.set("n", "<leader>l", ':b#<CR>', { desc = "Go to last used buffer", silent = true })
+vim.keymap.set("n", "<leader>;", ':b#<CR>', { desc = "Go to last used buffer", silent = true })
+vim.keymap.set("n", "<leader>j", ':bp<CR>', { desc = "Go to previous buffer", silent = true })
+vim.keymap.set("n", "<leader>k", ':bn<CR>', { desc = "Go to next buffer", silent = true })
+
+vim.keymap.set("n", "<C-S>", ':w<CR>', { desc = "Save current buffer" })
