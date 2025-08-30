87fa94efadece09217aaf6a1a2e9cd70cef3dfb2
BohdaR <bohdanshushval@gmail.com>
2025-08-30 22:15:04 +0300

    Add new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,5 @@ vim.keymap.set("n", "<leader>d", ':bd<CR>', { desc = "Destroy the current buffer
 
 vim.keymap.set("n", "<C-S>", ':w<CR>', { desc = "Save current buffer" })
 vim.keymap.set("n", "<leader>rc", ':&&<CR>', { desc = "Repeat command", silent = true })
+vim.keymap.set("n", "<leader>gb", ':G blame<CR>', { desc = "Show [g]it [b]lame", silent = true })
+vim.keymap.set("n", "<leader>tn", function() vim.opt.relativenumber = not vim.opt.relativenumber:get() end, { desc = "[T]oggle relative line [n]umbers" })
